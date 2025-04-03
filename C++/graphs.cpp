7b920c73049b3b84e7890a0a718e26a7695c8b88
Aryan <161190212+aryan-raj-05@users.noreply.github.com>
2025-04-03 16:35:15 +0000

    few changes

--- C++/graphs.cpp
+++ C++/graphs.cpp
@@ -8,6 +8,51 @@ using std::cout;
 using std::unordered_map;
 using std::vector;
 
+// Adjancency Matrix implementation of Graphs
+template <typename T>
+class graphAM {
+    vector<vector<int>> matrix;
+    unordered_map<T, int> table;
+    int cur = 0;
+    bool directed;
+
+public:
+    graphAM(int V, bool ordered = false) {
+        directed = ordered;
+        matrix.resize(V, vector<int>(V, 0));
+    }
+
+    void addVertex(T name) {
+        if (cur >= matrix.size()) {
+            cerr << "Not enough space\n";
+            return;
+        }
+        table.insert({name, cur++});
+    }
+
+    void joinV(T v1, T v2) {
+        if (table.find(v1) == table.end() || table.find(v2) == table.end()) {
+            cerr << "One or both vertex have not been added.\n";
+            return;
+        }
+
+        int idxv1 = table.at(v1);
+        int idxv2 = table.at(v2);
+        matrix[idxv1][idxv2] = 1;
+        if (!directed) {  matrix[idxv2][idxv1] = 1;  }
+    }
+
+    void printGraph() {
+        for(int i = 0; i < cur; i++) {
+            for(int j = 0; j < cur; j++) {
+                cout << matrix[i][j] << " ";
+            }
+            cout << '\n';
+        }
+    }
+};
+
+// Adjacency List implementation of Graphs
 template <typename T>
 class graphAL {
     vector<vector<int>> adjList;
@@ -69,17 +114,3 @@ public:
         cout << '\n';
     }
 };
-
-int main() {
-    graphAL<char> G;
-
-    vector<char> vertices = {'A', 'B', 'C', 'D'};
-    G.addVertex(vertices);
-
-    G.joinV('A', 'C');
-    G.joinV('A', 'D');
-    G.joinV('A', 'B');
-    G.joinV('C', 'B');
-
-    G.printGraph();
-}
