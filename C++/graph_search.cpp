aec39c6f43c7a6d940f180f34147d79c6a80ca00
Aryan <161190212+aryan-raj-05@users.noreply.github.com>
2025-04-10 10:11:02 +0000

    implemented BFS algorithm

---
+++ C++/graph_search.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+
+using std::cerr;
+using std::cout;
+using std::vector;
+using std::queue;
+using std::stack;
+
+class Graph {
+    vector<vector<int>> adjList;
+    int V;
+
+public:
+    Graph(int V) {
+        this->V = V;
+        adjList.resize(V);
+    }
+
+    void joinVertex(int p, int q) {
+        if (p >= V || q >= V) {
+            cerr << "Out of Bounds\n";
+            return;
+        }
+
+        adjList[p].push_back(q);
+        adjList[q].push_back(p);
+    }
+
+    void printGraph() {
+        for(int i = 0; i < adjList.size(); i++) {
+            cout << i << ": ";
+            for(int j = 0; j < adjList[i].size(); j++) {
+                cout << adjList[i][j] << ' ';
+            }
+            cout << '\n';
+        }
+    }
+
+    vector<int> BFS(int startV) {
+        vector<bool> visited(V, false);
+        queue<int> q;
+        vector<int> nodes;
+
+        q.push(startV);
+        visited[startV] = true;
+
+        while (!q.empty()) {
+            int curV = q.front();
+            q.pop();
+            nodes.push_back(curV);
+
+            for(int neighbour : adjList[curV]) {
+                if (!visited[neighbour]) {
+                    visited[neighbour] = true;
+                    q.push(neighbour);
+                }
+            }
+        }
+
+        return nodes;
+    }
+};
+
+int main() {
+    
+}
